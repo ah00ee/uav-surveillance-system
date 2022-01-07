00ea8d0fccdecf3ccc41c1a2425bc5e59267ed28
ah00ee <nhoahyoung@gmail.com>
2022-01-07 09:27:20 -0500

    set layouts and placeholders

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/map")
 async def client(request: Request):
     return templates.TemplateResponse("base.html", {"request":request})
 
-@app.get("/map")
+@app.get("/")
 async def map(request: Request):
     return templates.TemplateResponse("map.html", {"request":request})
