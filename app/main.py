0fd958876bb608d40d4cff183812677e52ea521c
ah00ee <nhoahyoung@gmail.com>
2022-01-10 12:11:25 -0500

    feat: add popup button

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import requests
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/map")
-async def client(request: Request):
-    return templates.TemplateResponse("base.html", {"request":request})
-
 @app.get("/")
 async def map(request: Request):
-    return templates.TemplateResponse("map.html", {"request":request})
+    return templates.TemplateResponse("map.html", {"request":request}) 
